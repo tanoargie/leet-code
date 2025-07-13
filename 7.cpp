15faa194a3dbbdeaa1810caf4eb5219f4bd6529a
Franco Serio <franco@samser.co>
2025-07-13 18:58:24 +0200

    feat: 7

---
+++ 7.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+  int reverse(int x) {
+    int res = 0;
+    while (x != 0) {
+      int digit = x % 10;
+      if (res > numeric_limits<int>::max() / 10 ||
+          res < numeric_limits<int>::min() / 10) {
+        return 0;
+      }
+      res = res * 10 + digit;
+      x = x / 10;
+    }
+    return res;
+  }
+};
