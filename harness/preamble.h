234b2ec8488f2b4f3d62fe8c789379a47898ff17
Franco Serio <franco@samser.co>
2026-09-04 21:22:53 +0200

    feat: automate tests and build

---
+++ harness/preamble.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "leet.h"
+
+#include <algorithm>
+#include <array>
+#include <cmath>
+#include <cstdint>
+#include <deque>
+#include <functional>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
