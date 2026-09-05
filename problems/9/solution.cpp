78c5bc684fbb5dfac3ee96867952ffa7abbb73e2
Franco Serio <franco@samser.co>
2026-09-05 14:47:33 +0200

    feat: problem 9

---
+++ problems/9/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+  bool isPalindrome(int x) {
+    if (x < 0) {
+      return false;
+    }
+
+    long long reversed = 0;
+    int original = x;
+
+    while (x > 0) {
+      int digit = x % 10;
+      reversed = (reversed * 10) + digit;
+      x = x / 10;
+    }
+
+    return reversed == original;
+  }
+};
